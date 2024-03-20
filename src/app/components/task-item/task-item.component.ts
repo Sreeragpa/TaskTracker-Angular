0e17a297700b8cf2811483a4c3265d5ec37b3e74
Sreerag PA <catchsreerag@gmail.com>
2024-03-20 13:24:10 +0530

    First commit

---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,12 @@
+import { Component,OnInit,Input } from '@angular/core';
+import { Task } from '../../Task';
+
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrl: './task-item.component.css'
+})
+export class TaskItemComponent {
+@Input() task: Task = {} as Task;
+
+}
